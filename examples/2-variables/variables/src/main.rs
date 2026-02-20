cf5420005422fb24e1edd2a84901c7a4222cd9a5
Alain Kelleter <alain.kelleter@kelleter.be>
2026-02-20 15:35:44 +0100

    Update main.rs

--- examples/2-variables/variables/src/main.rs
+++ examples/2-variables/variables/src/main.rs
@@ -3,7 +3,6 @@
 fn main() {
     let message = "Name: Alfredo, Weight: ";
     let weight = 190.0;
-
     let kilos = weight / 2.2;
     println!("{}{}", message, weight);
     println!("{}{}", message, kilos);
